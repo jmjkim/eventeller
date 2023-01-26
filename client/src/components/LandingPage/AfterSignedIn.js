7382f9dba71a53eae67021228d08463fc1e2ec00
jmjkmbm116p <jmjkmbm116p@jmjkmbm116ps-Laptop.local>
2023-01-26 13:14:22 -0800

    Changes landing page image1 url

--- client/src/components/LandingPage/AfterSignedIn.js
+++ client/src/components/LandingPage/AfterSignedIn.js
@@ -2,7 +2,7 @@ const AfterSignedIn = () => {
     return (
         <div className="signedin_img_container">
             <div className="signedin_img_subcontainer">
-                <img id="signedin_img1" src="https://www.creativeplansandservices.com/uploads/4/4/2/9/44292467/header_images/1421860351.jpg" alt="event1"></img> 
+                <img id="signedin_img1" src="https://cdn-cjhkj.nitrocdn.com/krXSsXVqwzhduXLVuGLToUwHLNnSxUxO/assets/images/optimized/rev-61fca30/wp-content/uploads/2020/10/AdobeStock_333205002-scaled.jpeg" alt="event1"></img> 
                 <img src="https://thumbs.dreamstime.com/b/friends-gathering-together-weekend-vector-buddies-sitting-couch-cafe-drinking-coffee-people-spend-time-playing-guitar-156466925.jpg" alt="event2"></img>
             </div>
         </div>
